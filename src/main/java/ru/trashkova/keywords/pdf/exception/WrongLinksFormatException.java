b918d0380309abafdde5281116384e98cae4f290
trashkova <trashkovaelina@gmail.com>
2022-07-06 02:02:38 +0300

    add payload layer

---
+++ src/main/java/ru/trashkova/keywords/pdf/exception/WrongLinksFormatException.java
@@ -0,0 +1,12 @@
+package ru.trashkova.keywords.pdf.exception;
+
+/**
+ * Исключение - файл с рекомендациями содержит некорректные данные
+ */
+public class WrongLinksFormatException extends RuntimeException {
+
+    public WrongLinksFormatException(String message) {
+        super(message);
+    }
+
+}
